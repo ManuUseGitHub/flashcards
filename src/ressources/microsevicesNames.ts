ceee9065d6de0a66d193de76138ad99e7a570cd3
manmdyalw@gmail.com <maze014.dev.prod@gmail.com>
2024-12-24 05:16:10 +0100

    API_VERSION names

--- src/ressources/microsevicesNames.ts
+++ src/ressources/microsevicesNames.ts
@@ -1,2 +1,2 @@
-export const API_VERSION_ID_JSON_SERVER = 'api/V1';
-export const API_VERSION_ID_ORM = 'api/V2';
+export const API_VERSION_ID_JSON_SERVER = 'json/V1';
+export const API_VERSION_ID_ORM = 'prisma/V1';
